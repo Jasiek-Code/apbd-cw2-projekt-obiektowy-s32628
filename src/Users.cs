260357abb8b56ea416434802c25ee52d33b5ad28
Jan Antczak <s32628@pjwstk.edu.pl>
2026-03-22 16:10:49 +0100

    removed abstract rent method from User classes

--- src/Users.cs
+++ src/Users.cs
@@ -7,25 +7,14 @@ public abstract class User(string name, string surname)
     public string Surname { get; set; } = surname;
 
     public abstract int MaxRentals { get; }
-    public abstract void Rent();
 }
 
 public class Student(string name, string surname) : User(name, surname)
 {
     public override int MaxRentals => 2;
-
-    public override void Rent()
-    {
-        
-    }
 }
 
 public class Employee(string name, string surname) : User(name, surname)
 {
     public override int MaxRentals => 5;
-
-    public override void Rent()
-    {
-        
-    }
 }
